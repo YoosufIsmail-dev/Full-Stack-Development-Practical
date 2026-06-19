8a2c626c6c3a711f11c4d6ffad072afe255d2ae7
Yoosuf Ismail <kiyas9148@gmail.com>
2026-06-19 08:41:38 +0530

    Add files via upload
    Even or Odd Number

---
+++ My Java Practice/01.Java-Operators-And-Control-Flow-Statements/Task02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even number.");
+        } else {
+            System.out.println(number + " is an Odd number.");
+        }
+
+        input.close();
+    }
+}
